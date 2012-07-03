517c2b3af5e6e58b40fadbdf79f537d2a8a38a5d
gk23 <aoaogk@gmail.com>
2012-07-03 17:39:49 +0800

    correct schema.rb, delete index.html.erb, default to session#login

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(:version => 20120703081958) do
     t.integer  "user_id"
     t.integer  "course_id"
     t.integer  "lesson_id"
-    t.integer  "exercise_idid"
+    t.integer  "exercise_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
