a0b9d022fc8fb2948584b5bf8f3f655beee7f60e
Administrator <bobby@ubuntu.ubuntu-domain>
2012-07-07 08:53:52 +0800

    add test data for courses

---
+++ db/migrate/20120707003133_courses_init.rb
@@ -0,0 +1,12 @@
+require 'active_record/fixtures'
+class CoursesInit < ActiveRecord::Migration
+  def up
+    down
+    dir = File.join(File.dirname(__FILE__),'dev_data')
+    ActiveRecord::Fixtures.create_fixtures(dir,'courses')
+  end
+
+  def down
+    Course.delete_all
+  end
+end
