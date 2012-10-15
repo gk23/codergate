6ff52f81ef24d51caa32f8b03d1c949faf4f0931
gk23 <aoaogk@gmail.com>
2012-10-15 21:12:30 +0800

    ui for the button of creating course.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120710015642) do
+ActiveRecord::Schema.define(:version => 20120707003133) do
 
   create_table "courses", :force => true do |t|
     t.string   "name",       :limit => 80
